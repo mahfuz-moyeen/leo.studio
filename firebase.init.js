7e4e7c012b177e9230e73d7401e4cc9a298ed4b2
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-17 18:06:10 +0600

    environment variable

--- firebase.init.js
+++ firebase.init.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUhHaektDnraxDMah7GwDe1xV4haSsQ20",
-  authDomain: "leo-studio-5d849.firebaseapp.com",
-  projectId: "leo-studio-5d849",
-  storageBucket: "leo-studio-5d849.appspot.com",
-  messagingSenderId: "629500141808",
-  appId: "1:629500141808:web:25235da1f7d32a4132a235"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
