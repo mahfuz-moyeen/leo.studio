5f6e1b42b8c7919861ea790977a6211df772f219
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-17 15:20:03 +0600

    navbar router

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import logo from '../../image/logo-dark.png'
+import {Link} from 'react-router-dom';
+import logo from '../../image/logo-dark.png';
 
 const MenuBar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand as={Link} to="/">
                     <img
                         src={logo}
                         width="60"
@@ -18,13 +19,13 @@ const MenuBar = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link >Home</Nav.Link>
-                        <Nav.Link >Blog</Nav.Link>
-                        <Nav.Link >About</Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">Register</Nav.Link>
-                        <Nav.Link href="#memes">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
