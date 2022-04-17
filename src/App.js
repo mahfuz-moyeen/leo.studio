80da79ceb3f4fd8c0bf23d842fddfb357953074c
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-17 14:50:48 +0600

    add menubar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import MenuBar from './components/MenuBar/MenuBar';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-danger'>text</h1>
+      <MenuBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
