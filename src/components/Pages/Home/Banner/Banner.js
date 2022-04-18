67b6a526e6be009dd75fdbf717c3dc8baac67abc
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-18 14:48:25 +0600

    fix responsive

--- src/components/Pages/Home/Banner/Banner.js
+++ src/components/Pages/Home/Banner/Banner.js
@@ -1,19 +1,21 @@
-import { Carousel } from 'bootstrap';
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import banner2 from '../../../../image/banner/banner-2.jpg';
 import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className='d-flex my-3 container gap-2'>
-            <div className='col-md-6 banner-text-section text-center'>
-                <h1 className='fs-1 text-primary'>Every moment of your life is beautiful.</h1>
-                <p className='my-3 p-3'>Your each movement,your each emotion is nothing but a beautiful picture and I am just want to hold on those moments with my camera and make it a piece of art with  editing.</p>
-                
-            </div>
-            <div className='col-md-6 banner-img-section'>
-                <img className='img-fluid' src={banner2} alt="" />
-            </div>
+        <div className='my-3 container'>
+            <Row xs={1} md={2} className="g-5">
+                <Col className='col-12 col-md-6 banner-text-section text-center order-2 order-md-1'>
+                    <h1 className='fs-1 text-primary'>Every moment of your life is beautiful.</h1>
+                    <p className='my-3 p-3'>Your each movement,your each emotion is nothing but a beautiful picture and I am just want to hold on those moments with my camera and make it a piece of art with  editing.</p>
+                </Col>
+
+                <Col className='banner-img-section order-1 order-md-2'>
+                    <img className='img-fluid' src={banner2} alt="" />
+                </Col>
+            </Row>
         </div>
     );
 };
